8f0b088886ba3064bdecb33a35d978748b740af0
jiriks74 <jiri@stefka.eu>
2024-03-06 23:07:22 +0100

    feat: basic nvim-dap setup

--- nvim/ftplugin/c.lua
+++ nvim/ftplugin/c.lua
@@ -14,5 +14,29 @@ vim.lsp.start {
   cmd = { 'clangd' },
   root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
   capabilities = require('user.lsp').make_client_capabilities(),
-  -- capabilities = { offsetEncoding = "utf-8" },
+}
+
+local dap = require("dap")
+
+if require('user.file_exists').file_exists(vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]) .. "/.vscode/launch.json") then
+  require("dap.ext.vscode").load_launchjs(nil, { cppdbg = { "c", "cpp", "asm" } })
+end
+
+dap.adapters.gdb = {
+  type = "executable",
+  command = "gdb",
+  args = { "-i", "dap" }
+}
+
+dap.configurations.c = {
+  {
+    name = "Launch",
+    type = "gdb",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = "${workspaceFolder}",
+    stopAtBeginningOfMainSubprogram = false,
+  },
 }
